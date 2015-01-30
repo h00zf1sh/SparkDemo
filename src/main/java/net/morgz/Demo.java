1aa423c1aead5af5c6d1d2979d0e4d4340955579
Jon Morgan <jon.morgan@boxuk.com>
2015-01-30 22:17:39 +0000

    Initial commit

---
+++ src/main/java/net/morgz/Demo.java
@@ -0,0 +1,18 @@
+package net.morgz;
+
+/**
+ * Created by Morgz on 30/01/2015.
+ */
+public class Demo
+{
+    private void start()
+    {
+        System.out.println("Well, made a start! :-)");
+    }
+
+    public static void main(String args[])
+    {
+        Demo demo = new Demo();
+        demo.start();
+    }
+}
